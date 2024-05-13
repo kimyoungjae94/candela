2337d3c5072662c2be7d3642d50dc96caab43f78
kimyoungjae <osung2110.yk@gmail.com>
2024-05-13 22:57:34 +0900

    commit

---
+++ src/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCXF7Z2KU-rdgGE7n6ZP8xWYpOnWApSto0',
+  authDomain: 'candela-debe0.firebaseapp.com',
+  projectId: 'candela-debe0',
+  storageBucket: 'candela-debe0.appspot.com',
+  messagingSenderId: '703466379141',
+  appId: '1:703466379141:web:ed8d2612a354fef79896ec',
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
